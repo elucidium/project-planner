76085f8fc1f80f6506184ca87f9bc4e314cb5ff1
elucidium <xun.ruiran@gmail.com>
2020-07-12 22:02:46 -0400

    minor refactoring

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -10,93 +10,97 @@ function showSignup() {
     $('body').html(signupOutput);
 }
 
+function renderCourses(snapshot) {
+    var courseDB = snapshot.child('courses');
+    $('#courses-count').html(courseDB.numChildren());
+    var courseData = [];
+    courseDB.forEach(child => {
+        var data = child.val();
+        if (data['assignmentsCount'] == 1)
+            data['plural'] = '';
+        else
+            data['plural'] = 's';
+        data['courseId'] = child.key;
+        courseData.push(data);
+    });
+    var courseTmpl = $.templates('#courseTmpl');
+    var courseOutput = courseTmpl.render(courseData);
+    $('#courses').html(courseOutput);
+}
+
+function renderAssignments(snapshot) {
+    var numUpcomingAssignments = 0;
+    var assignDB = snapshot.child('assignments');
+    var upAssignData = [];
+    var compAssignData = [];
+    assignDB.forEach(child => {
+        var data = child.val();
+        data['assignmentId'] = child.key;
+        if (data['milestonesCount'] == 1)
+            data['plural'] = '';
+        else
+            data['plural'] = 's';
+        if (!data['isCompleted']) {
+            upAssignData.push(data);
+            numUpcomingAssignments++;
+        } else {
+            compAssignData.push(data);
+        }
+    });
+    $('#assignments-count').html(numUpcomingAssignments);
+    var upAssignTmpl = $.templates('#upcomingAssignmentTmpl');
+    var upAssignOutput = upAssignTmpl.render(upAssignData);
+    $('#upcoming-assignments').html(upAssignOutput);
+    var compAssignTmpl = $.templates('#completedAssignmentTmpl');
+    var compAssignOutput = compAssignTmpl.render(compAssignData);
+    $('#completed-assignments').html(compAssignOutput);
+}
+
+function renderMilestones(snapshot) {
+    var numUpcomingMilestones = 0;
+    var mileDB = snapshot.child('milestones');
+    var upMileData = [];
+    var compMileData = [];
+    mileDB.forEach(child => {
+        var data = child.val();
+        data['milestoneId'] = child.key;
+        if (!data['isCompleted']) {
+            upMileData.push(data);
+            numUpcomingMilestones++;
+        } else {
+            compMileData.push(data);
+        }
+    });
+    $('#milestones-count').html(numUpcomingMilestones);
+    var upMileTmpl = $.templates('#upcomingMilestoneTmpl');
+    var upMileOutput = upMileTmpl.render(upMileData);
+    $('#upcoming-milestones').html(upMileOutput);
+    var compMileTmpl = $.templates('#completedMilestoneTmpl');
+    var compMileOutput = compMileTmpl.render(compMileData);
+    $('#completed-milestones').html(compMileOutput);
+}
+
 function showMain() {
     var user = firebase.auth().currentUser;
     var email, uid;
     if (user != null) {
         email = user.email;
         uid = user.uid;
-        console.log('email: ' + email);
-        console.log('uid: ' + uid);
     }
     var portalTmpl = $.templates('#portalTmpl');
     var portalOutput = portalTmpl.render({username: email});
     $('body').html(portalOutput);
     var db = firebase.database().ref(uid);
-    db.on('value', function(snapshot) {
-        var courseDB = snapshot.child('courses');
-        $('#courses-count').html(courseDB.numChildren());
-        var courseData = [];
-        courseDB.forEach(function (child) {
-            var data = child.val();
-            data['courseId'] = child.key;
-            courseData.push(data);
-            console.log(child.val().courseCode);
-        });
-        console.log(snapshot.child('courses').toJSON());
-        console.log(snapshot.key);
-        var courseTmpl = $.templates('#courseTmpl');
-        var courseOutput = courseTmpl.render(courseData);
-        $('#courses').html(courseOutput);
-
-        var numUpcomingAssignments = 0;
-        var assignDB = snapshot.child('assignments');
-        var upAssignData = [];
-        var compAssignData = [];
-        assignDB.forEach(function (child) {
-            var data = child.val();
-            data['assignmentId'] = child.key;
-            if (data['milestonesCount'] == 1)
-                data['plural'] = '';
-            else
-                data['plural'] = 's';
-            console.log('plural: ' + data['plural']);
-            if (!data['isCompleted']) {
-                console.log(child.key);
-                upAssignData.push(data);
-                numUpcomingAssignments++;
-            } else {
-                compAssignData.push(data);
-            }
-        });
-        $('#assignments-count').html(numUpcomingAssignments);
-        var upAssignTmpl = $.templates('#upcomingAssignmentTmpl');
-        var upAssignOutput = upAssignTmpl.render(upAssignData);
-        $('#upcoming-assignments').html(upAssignOutput);
-        var compAssignTmpl = $.templates('#completedAssignmentTmpl');
-        var compAssignOutput = compAssignTmpl.render(compAssignData);
-        $('#completed-assignments').html(compAssignOutput);
-
-        var numUpcomingMilestones = 0;
-        var mileDB = snapshot.child('milestones');
-        var upMileData = [];
-        var compMileData = [];
-        mileDB.forEach(function (child) {
-            var data = child.val();
-            data['milestoneId'] = child.key;
-            if (!data['isCompleted']) {
-                upMileData.push(data);
-                numUpcomingMilestones++;
-            } else {
-                compMileData.push(data);
-            }
-        });
-        $('#milestones-count').html(numUpcomingMilestones);
-        var upMileTmpl = $.templates('#upcomingMilestoneTmpl');
-        var upMileOutput = upMileTmpl.render(upMileData);
-        $('#upcoming-milestones').html(upMileOutput);
-        var compMileTmpl = $.templates('#completedMilestoneTmpl');
-        var compMileOutput = compMileTmpl.render(compMileData);
-        $('#completed-milestones').html(compMileOutput);
-    })
-    console.log(db);
+    db.on('value', snapshot => {
+        renderCourses(snapshot);
+        renderAssignments(snapshot);
+        renderMilestones(snapshot);
+    });
 }
 
 function submitCourse() {
     var courseCode = $('#course-code').val();
     var courseName = $('#course-name').val();
-    console.log('code: ' + courseCode);
-    console.log('name: ' + courseName);
     var user = firebase.auth().currentUser;
     var db = firebase.database().ref(user.uid).child('courses');
     var newRef = db.push({
@@ -104,7 +108,6 @@ function submitCourse() {
         courseCode: courseCode,
         courseName: courseName
     });
-    console.log(newRef.key);
     $('#course-modal').modal('toggle');
 }
 
@@ -112,10 +115,8 @@ function loadCourseOptions() {
     $('#course-select').empty();
     var user = firebase.auth().currentUser;
     var db = firebase.database().ref(user.uid).child('courses');
-    db.on('value', function (snapshot) {
-        snapshot.forEach(function (child) {
-            console.log(child.key);
-            console.log(child.val().courseCode);
+    db.on('value', snapshot => {
+        snapshot.forEach(child => {
             var data = child.val();
             let opt = document.createElement('option');
             opt.value = child.key;
@@ -127,12 +128,11 @@ function loadCourseOptions() {
 }
 
 function loadAssignmentOptions() {
-    console.log('ran loadAssignmentOptions');
     $('#assignment-select').empty();
     var user = firebase.auth().currentUser;
     var db = firebase.database().ref(user.uid).child('assignments');
-    db.on('value', function (snapshot) {
-        snapshot.forEach(function (child) {
+    db.on('value', snapshot => {
+        snapshot.forEach(child => {
             var data = child.val();
             if (!data['isCompleted']) {
                 let opt = document.createElement('option');
@@ -155,7 +155,7 @@ function submitAssignment() {
     var time = $('#assignment-time').val();
     var courseCode;
     db.child('courses/' + courseId).once('value')
-    .then(function (snapshot) {
+    .then(snapshot => {
         courseCode = snapshot.val().courseCode;
     }).then(() => {
         db.child('assignments').push({
@@ -169,9 +169,9 @@ function submitAssignment() {
         });
     });
     db.child('courses/' + courseId).once('value')
-    .then(function (snapshot) {
+    .then(snapshot => {
         return snapshot.val().assignmentsCount;
-    }).then((currNumAssignments) => {
+    }).then(currNumAssignments => {
         db.child('courses/' + courseId)
         .update({assignmentsCount: currNumAssignments + 1});
     });
@@ -183,9 +183,9 @@ function deleteAssignment(assignmentId, courseId) {
     var db = firebase.database().ref(user.uid);
     db.child('assignments/' + assignmentId).remove();
     db.child('courses').child(courseId).once('value')
-    .then(function (snapshot) {
+    .then(snapshot => {
         return snapshot.val().assignmentsCount;
-    }).then((currNumAssignments) => {
+    }).then(currNumAssignments => {
         db.child('courses/' + courseId)
         .update({assignmentsCount: currNumAssignments - 1});
     });
@@ -214,7 +214,7 @@ function submitMilestone() {
     var time = $('#milestone-time').val();
     var assignmentName;
     db.child('assignments/' + assignmentId).once('value')
-    .then(function (snapshot) {
+    .then(snapshot => {
         assignmentName = snapshot.val().assignmentName;
     }).then(() => {
         db.child('milestones').push({
@@ -227,9 +227,9 @@ function submitMilestone() {
         });
     });
     db.child('assignments/' + assignmentId).once('value')
-    .then(function (snapshot) {
+    .then(snapshot => {
         return snapshot.val().milestonesCount;
-    }).then((currNumMilestones) => {
+    }).then(currNumMilestones => {
         db.child('assignments/' + assignmentId)
         .update({milestonesCount: currNumMilestones + 1});
     });
@@ -243,7 +243,7 @@ function deleteMilestone(milestoneId, assignmentId) {
     db.child('assignments').child(assignmentId).once('value')
     .then(function (snapshot) {
         return snapshot.val().milestonesCount;
-    }).then((currNumMilestones) => {
+    }).then(currNumMilestones => {
         db.child('assignments/' + assignmentId)
         .update({milestonesCount: currNumMilestones - 1});
     });
@@ -273,7 +273,7 @@ function signup() {
     var email = document.getElementsByName('email')[0].value;
     var password = document.getElementsByName('password')[0].value;
     firebase.auth().createUserWithEmailAndPassword(email, password)
-    .catch(function(error) {
+    .catch(error => {
         console.log(error.code);
         console.log(error.message);
     });
@@ -283,34 +283,28 @@ function signin() {
     var email = document.getElementsByName('email')[0].value;
     var password = document.getElementsByName('password')[0].value;
     firebase.auth().signInWithEmailAndPassword(email, password)
-    .catch(function(error) {
+    .catch(error => {
         console.log(error.code);
         console.log(error.message);
     });
 }
 
 function signout() {
-    firebase.auth().signOut().then(function() {
-        console.log("Logged out!");
-    }, function(error) {
+    firebase.auth().signOut()
+    .catch(error => {
         console.log(error.code);
         console.log(error.message);
     });
 }
 
 function initialize() {
-    firebase.auth().onAuthStateChanged(function (user) {
+    firebase.auth().onAuthStateChanged(user => {
         if (user) {
             showMain();
-            console.log('loaded portal');
         } else {
             showLogin();
-            console.log('loaded login template');
         }
     });
 }
 
-window.onload = function () {
-    console.log('ran onload');
-    initialize();
-}
+window.onload = initialize;
